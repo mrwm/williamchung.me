8a52d0e80198308ea6ebc968e8ffd14fbc5d2c1a
William Chung <wiiliamchung360@gmail.com>
2019-07-02 14:37:03 -0700

    +GID56 Course intro page +footer nav bar for class links

--- image.js
+++ image.js
@@ -1,5 +1,6 @@
 var modal = document.getElementById("imgModal");
 
+// This works with the img tags
 function imageThis(_this){
   var captionText = document.getElementById("caption");
   var modalImg = document.getElementById("imgMod");
@@ -7,6 +8,17 @@ function imageThis(_this){
   modalImg.src = _this.src;
   captionText.innerHTML = _this.alt;
 }
+
+
+// This works for the picture tags and uses the fallback jpg/png
+function imageThisP(_this){
+  var captionText = document.getElementById("caption");
+  var modalImg = document.getElementById("imgMod");
+  modal.style.display = "block";
+  modalImg.src = _this.childNodes[3].src;
+  captionText.innerHTML = _this.childNodes[3].alt;
+}
+
 
 var closeScrn = document.getElementById("closeScreen");
 var closeBtn = document.getElementById("closeBtn");
