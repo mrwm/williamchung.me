c3cfe3aa189ab7880e76387b1e7f45ec743277dc
William Chung <wiiliamchung360@gmail.com>
2019-11-24 20:38:53 -0800

    added labels to visualization

--- art175/art101/project4/proj4/sketch.js
+++ art175/art101/project4/proj4/sketch.js
@@ -9,15 +9,15 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
 
-  noStroke();
+//  noStroke();
 
 }
 
-let sec = 6;
+let sec = 4;
 let txt="starting in "
 let startDrawing = false;
 function draw() {
-  background(0);
+  background('#16161d');
   fill(255);
 
   if (frameCount % 60 == 0){
@@ -36,15 +36,45 @@ function draw() {
     text(txt, width/2, height/2);
 }
 
+
+let prevHeight, prevWidth, rectWidth, rectHeight, elev, fillC;
+let distY;
 function drawRects(){
   for(let i=0; i<data.length; i++){
-    let rectWidth = map(i, 0, data.length, 0, width); //map the rect width to available width
-    let rectHeight = map(data[i].distance, maxDistArr[0]/2, maxDistArr[(maxDistArr.length - 1)], 0, height); //map height to distance
+    rectWidth = i*(width/data.length); //map the rect width to available width
+    rectHeight = map(data[i].distance, maxDistArr[0]/2, maxDistArr[(maxDistArr.length - 1)], 0, height); //map height to distance
+    if(i!=0){
+      prevWidth = (i-1)*(width/data.length); //map the rect width to available width
+      prevHeight = map(data[i-1].distance, maxDistArr[0]/2, maxDistArr[(maxDistArr.length - 1)], 0, height); //map height to distance
+    }
 
-    let elev = data[i].elev_high - data[i].elev_low;
-    let fillC = map(elev, maxElevArr[0], maxElevArr[(maxElevArr.length - 1)], 0, 255);
+    elev = data[i].elev_high - data[i].elev_low;
+    fillC = map(elev, maxElevArr[0], maxElevArr[(maxElevArr.length - 1)], 0, 255);
+
     fill(fillC,100,100);
+    stroke(fillC,100,100);
+    strokeWeight(4);
 
-    rect(i*rectWidth, height - rectHeight, rectWidth, rectHeight);
+    if(i!=0){
+      line(rectWidth, height - rectHeight, prevWidth, height - prevHeight);
+    }
+
+
+    if ((mouseX > prevWidth) && (mouseX < rectWidth)
+    ){
+      distY = dist(mouseX, height - rectHeight, mouseX, height - prevHeight);
+      if(elev > 500)
+        fill('#e9e9e2');
+      //rect(mouseX,mouseY,-10,-10);
+      noStroke();
+      if (mouseX > width/2)
+        textAlign(RIGHT);
+      if (mouseX < width/2)
+        textAlign(LEFT);
+      text(prevHeight+"m (distance)",mouseX,height - prevHeight);
+      text(elev+"m (elevation)",mouseX,mouseY-10);
+      //frameRate(1);
+      //console.log(elev);
+    }
   }
 }
