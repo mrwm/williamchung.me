7fbd6a1feee99c117e7bac47b98b18ed869d1465
william <mrwm@wksp2>
2021-04-01 21:17:25 -0700

    move all the pages into one

---
+++ art103/js/script.js
@@ -0,0 +1,37 @@
+let pageDict = {
+  "Home":               0,
+  "Oblique Strategies": 1,
+  "Reading #1":         2,
+  "Tessellation":       3,
+  "CAD Fun":            4,
+  "Reading #2":         5,
+};
+
+function page(x){
+  // Remove all currentNav classes from the page
+  let nav = document.getElementsByClassName("currentNav");
+  for (i=0; i < nav.length; i++){
+    nav[i].classList.remove("currentNav");
+  }
+  // Then add the currentNav to the item clicked
+  x.classList.add("currentNav");
+
+  let inHTML = x.innerHTML;
+
+  let targetID = null;
+  targetID = pageDict[inHTML];
+
+  let other = document.getElementsByClassName("content");
+  let target = document.getElementById(targetID);
+
+  // Hide all page content
+  for (i=0; i < other.length; i++){
+    other[i].classList.add("hidden");
+  }
+  // Then show the correnct content
+  target.classList.remove("hidden");
+
+  document.getElementsByTagName("details")[0].removeAttribute("open");
+}
+
+
