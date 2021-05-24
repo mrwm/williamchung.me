efbab4fae67f0bb7d740a8d2c3c6895fb64cb532
william <mrwm@wksp2>
2021-05-23 22:50:25 -0700

    finalize (103) website and prep for 197 work

--- art103/page.js
+++ art103/page.js
@@ -1,21 +1,23 @@
 /*
-  Compare the text within the anchor tags to link index's,
-  then use the index to grab the iframe link. 
-
-  pageDict -> linkDict
-
+  Make sure that the text value here match the value inside the links.
+  To get the google doc link, go to File -> Publish to the web -> embed
+  and copy the iframe code.
+  
+  Then take the iframe code and grab the link inside the src=... and paste it
+  to the number that corresponds to the dictionary.
 */
-
-let pageDict = {
+let pageDict = { // Compare the text within the anchor tags to link index's
   "Home":               0,
   "Oblique Strategies": 1,
-  "Reading #1":         2,
+  "Reading 1":          2,
   "Tessellation":       3,
   "CAD Fun":            4,
-  "Reading #2":         5,
+  "Reading 2":          5,
   "Signs":              6,
+  "Reading 3":          7,
+  "Artifact":           8,
 };
-let linkDict = {
+let linkDict = { // then use the index to grab the iframe link
   0: "#",
   1: "https://docs.google.com/document/d/e/2PACX-1vROtkArryPdhjb8y29kLv2U4bkA50-j5L8oB6LWLLHWXBWLzKXQhb4BHopTfag-CqnG5ZdCvUVHALHR/pub?embedded=true",
   2: "https://docs.google.com/document/d/e/2PACX-1vTZ17fHypkO-UfLQcB1qvqsmg2CZeI8caZ6Dszkf3c98cTo3_G1SGp92-DduA-hC3cPSj1zG27Os1hU/pub?embedded=true",
@@ -23,13 +25,16 @@ let linkDict = {
   4: "https://docs.google.com/document/d/e/2PACX-1vSZksBqBl6vThp2JbtW3AMPnJlkuu3CUeAyGxrBT5R5hPip-4C9ghV1fRKwLoysDlj2oybmxyw9s3Mt/pub?embedded=true",
   5: "https://docs.google.com/document/d/e/2PACX-1vQUUCfq3-FNqE_xZ6AOJj24QDFbTcX2EAGp5KFGrWbDyOSlNBGQ1_Iua3AdHJxeqiVN__4oi_6y3_O3/pub?embedded=true",
   6: "https://docs.google.com/document/d/e/2PACX-1vQQnOVDFhbb2nrKC5hT1lt_FdGs6rWKaFynWDMd2V6idQEMK8Cs7cJf0DOBKIcwjXmgTkc4PaLe0n8S/pub?embedded=true",
+  7: "https://docs.google.com/document/d/e/2PACX-1vTQDK2ZSwHNA-oyupIqs2FY-BeZcuUgSdY-OCkc390tI8mY2yoAc3u7S4ZMO92SfwUvgesuLxNbG3Ky/pub?embedded=true",
+  8: "https://docs.google.com/document/d/e/2PACX-1vRuDHwPpOtkCa0NEkwaSc1YgdEZR5d0xxlMBJ8b4alq-ins4kPkAk02N6VS214qf4tRtNOKi4WnVkFO/pub?embedded=true",
 };
-
 function page(x){
   /*
     This function show/hides the homepage text and frame according to
     what is clicked. If a frame is shown, this function will change the
     frame source according to what is selected.
+
+    eg: If page(1) is called, then the page 1 will be loaded.
   */
 
   // Remove all currentNav classes from the page
@@ -52,7 +57,7 @@ function page(x){
   }
   else {
     // Set targetID to whatever value x was passed
-    targetID = x;
+    targetID = pageDict[decodeURI(x)]; //decodeURI() is used to change "%20" to spaces
     document.getElementById(targetID).classList.add("currentNav");
   }
 
@@ -84,10 +89,14 @@ function page(x){
   //location.reload(true);
 }
 
+
 function initialize(){
   /*
     This function is loaded whenever the page is loaded. This starts by
-    checking the URL for any hashes (#) and sets the page accordingly
+    checking the URL for any hashes (#) and sets the page accordingly.
+
+    eg: If the page is loaded with #reading1 in the URL,
+        the page reading1 will be loaded
   */
 
   // Look for any hashes in the URL
@@ -107,7 +116,7 @@ window.onhashchange = initialize;
 function detailOpen(stat){
   /*
     This function counts the number of links and subtracts the combined
-    height of those links from the frame height to keep the ifram in view
+    height of those links from the frame height to keep the iframe in view
   */
 
   // Grab the iframe
@@ -122,7 +131,8 @@ function detailOpen(stat){
     frame.style.height = "calc(100vh - var(--font-size) * (2.6 * " + aCount + ")";
   }
   else {
-//    frame.style.height = "calc(100vh - " + fontSizeFloat + "em)";
+    //frame.style.height = "calc(100vh - " + fontSizeFloat + "em)";
     frame.style.height = "calc(100vh - var(--font-size) * 2.5";
   }
 }
+
